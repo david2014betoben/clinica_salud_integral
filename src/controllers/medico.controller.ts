6635bb82cd2ce09b3f5fe5e53ed366ed3ce64e28
david2014betoben <davidgerardonunezrojas@gmail.com>
2026-09-03 18:35:16 -0400

    feat:actualizando crud medicos

--- src/controllers/medico.controller.ts
+++ src/controllers/medico.controller.ts
@@ -12,3 +12,99 @@ export const getMedicos = async (
     res.status(500).json({ message: error });
   }
 };
+
+export const postMedico = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { nombre, ap_paterno, ap_materno, telefono, email, especialidad_id } =
+      req.body;
+
+    if (!nombre || !ap_paterno || !especialidad_id) {
+      res.status(400).json({
+        err: "faltan campos obligatorios",
+      });
+      return;
+    }
+
+    const newMedico = await medicoModel.create(
+      nombre,
+      ap_paterno,
+      especialidad_id,
+      ap_materno,
+      telefono,
+      email,
+    );
+
+    res.status(201).json({
+      message: "Médico creado con éxito",
+      data: newMedico,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+};
+
+export const putMedico = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      res.status(400).json({
+        message: "El ID debe ser un número válido",
+      });
+      return;
+    }
+
+    const { nombre, ap_paterno, ap_materno, telefono, email, especialidad_id } =
+      req.body;
+
+    const updateMedico = await medicoModel.update(id, {
+      nombre,
+      ap_paterno,
+      ap_materno,
+      telefono,
+      email,
+      especialidad_id,
+    });
+
+    res.status(200).json({
+      message: "Médico actualizado con éxito",
+      data: updateMedico,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error del servidor",
+    });
+  }
+};
+
+export const deleteMedico = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+      res.status(400).json({
+        message: "El ID debe ser un número válido",
+      });
+      return;
+    }
+
+    await medicoModel.delete(id);
+
+    res.status(200).json({
+      message: "Médico eliminado exitosamente",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Error del servidor",
+    });
+  }
+};
--- src/models/medico.model.ts
+++ src/models/medico.model.ts
@@ -3,10 +3,6 @@ import prisma from "../config/prisma";
 export const medicoModel = {
   findAll: async () => {
     return await prisma.medico.findMany({
-      /*orderBy: { id: "asc" },
-      include: {
-        especialidad: true,
-      },*/
       select: {
         nombre: true,
         ap_paterno: true,
@@ -17,6 +13,49 @@ export const medicoModel = {
           },
         },
       },
+    });
+  },
+
+  create: async (
+    nombre: string,
+    ap_paterno: string,
+    especialidad_id: number,
+    ap_materno?: string,
+    telefono?: string,
+    email?: string,
+  ) => {
+    return await prisma.medico.create({
+      data: {
+        nombre,
+        ap_paterno,
+        especialidad_id,
+        ...(ap_materno !== undefined && { ap_materno }),
+        ...(telefono !== undefined && { telefono }),
+        ...(email !== undefined && { email }),
+      },
+    });
+  },
+
+  update: async (
+    id: number,
+    data: {
+      nombre?: string;
+      ap_paterno?: string;
+      ap_materno?: string;
+      telefono?: string;
+      email?: string;
+      especialidad_id?: number;
+    },
+  ) => {
+    return await prisma.medico.update({
+      where: { id },
+      data,
+    });
+  },
+
+  delete: async (id: number) => {
+    return await prisma.medico.delete({
+      where: { id },
     });
   },
 };
